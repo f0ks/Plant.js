36fb3ea7cbd9f68a8ca6a548cea3a3fd3763d15d
Albert Khamidullin <albert.khm@gmail.com>
2012-03-15 20:43:30 +0600

    second enemy templorary added

--- code.js
+++ code.js
@@ -1,5 +1,8 @@
 var example = {
 
+    enemies: [],
+    bullets: [],
+
     isLeftPressed : false,
     isRightPressed : false,
     isUpPressed : false,
@@ -93,7 +96,41 @@ var example = {
             color: '#aaf',
             zindex: 0
         });
+//////////////////// st
+        var myEnemy2 = new plant.Sprite({
+            x: 40, 
+            y: 467, 
+            width: 14,
+            height: 13,
+            frameWidth: 14,
+            frameHeight: 13,
+            xFrame: 0,
+            yFrame: 0,
+            src: 'enemy.png', 
+            zindex: 2,
+        });
 
+        // set some custom properties
+        myEnemy2.isGoRight = true;
+        myEnemy2.shotPosition = 0;
+        myEnemy2.isGunLoaded = false;
+        myEnemy2.isShot = false;
+        myEnemy2.isDying = false;
+        myEnemy2.isDead = false;
+        myEnemy2.shotPosition = plant.Random(10, 300);
+
+        var myEnemyBullet2 = new plant.Rectangle({
+            width: 1,
+            height: 5,
+            color: '#aaf',
+            zindex: 0
+        });
+
+        example.enemies[0] = myEnemy2;
+        example.bullets[0] = myEnemyBullet2;
+        myScene.addChild(example.enemies[0]);
+        myScene.addChild(example.bullets[0]);
+////////////// end
         
         myScene.addChild(myTxt);
         myScene.addChild(myTxt2);
@@ -135,6 +172,14 @@ var example = {
                 myEnemyBullet.x = myEnemy.x + 8;
                 myEnemyBullet.y = myEnemy.y + 1;
             }
+
+            //// st
+            // stick bullet to enemy
+            if (!example.enemies[0].isShot) {
+                example.bullets[0].x = example.enemies[0].x + 8;
+                example.bullets[0].y = example.enemies[0].y + 8;
+            }
+            //// end
             
             // hit on enemy check
             if (plant.Collision(myBomb, myEnemy) && !myEnemy.isDead) {
@@ -143,6 +188,16 @@ var example = {
                 myEnemy.xFrame = 0;
                 example.score += 10;
             }
+
+            //// st
+            // hit on enemy check
+            if (plant.Collision(myBomb, example.enemies[0]) && !example.enemies[0].isDead) {
+                example.enemies[0].isDying = true;
+                example.enemies[0].yFrame = 1;
+                example.enemies[0].xFrame = 0;
+                example.score += 10;
+            }
+            //// end
             
             // hit on player check
             if (plant.Collision(myPlayer, myEnemyBullet)) {
@@ -155,6 +210,20 @@ var example = {
 
                 myTxt2.text = 'ПЕДИК';
             }
+
+            ///// st
+            // hit on player check
+            if (plant.Collision(myPlayer, example.bullets[0])) {
+
+                plant.isDying = true;
+
+                myPlayerExplosion.x = myPlayer.x - 10;
+                myPlayerExplosion.y = myPlayer.y - 5;
+                myPlayerExplosion.visible = true;
+
+                myTxt2.text = 'ПЕДИК';
+            }
+            ///// end
 
             // player animation
             if (!plant.isDying) {
@@ -193,6 +262,25 @@ var example = {
                 }
             }
 
+            ///// st
+            // enemy animation
+            if (!example.enemies[0].isDying) {
+                // normal animation
+                if (example.enemies[0].xFrame > 1) {
+                    example.enemies[0].xFrame = 0;
+                } else {
+                    example.enemies[0].xFrame++;
+                }
+            } else if (example.enemies[0].isDying && !example.enemies[0].isDead) {
+                // dying animation
+                if (example.enemies[0].xFrame < 2) {
+                    example.enemies[0].xFrame++;
+                } else {
+                    example.enemies[0].isDead = true;
+                }
+            }
+            ///// end
+
             // move enemy
             if (!myEnemy.isDying) {
                 if (myEnemy.isGoRight) {
@@ -216,6 +304,31 @@ var example = {
                 myEnemy.isGunLoaded = false;
             }
 
+            //// st
+            // move enemy
+            if (!example.enemies[0].isDying) {
+                if (example.enemies[0].isGoRight) {
+                    example.enemies[0].x += 2;
+                } else {
+                    example.enemies[0].x -= 2;
+                }
+
+                if (example.enemies[0].x > 300) {
+                    example.enemies[0].isGoRight = false;
+                } 
+                if (example.enemies[0].x < 10) {
+                    example.enemies[0].isGoRight = true;
+                } 
+            }
+
+            // enemy shot
+            if(example.enemies[0].isGunLoaded) {
+                // set random shot position
+                example.enemies[0].shotPosition = plant.Random(10, 300);
+                example.enemies[0].isGunLoaded = false;
+            }
+            //// end
+
             // bullet animation
             if (myEnemy.isShot) {
                 myEnemyBullet.y -= 12;
@@ -231,6 +344,24 @@ var example = {
                 // reload a gun
                 myEnemy.isGunLoaded = true;
             }
+
+            /// st
+            // bullet animation
+            if (example.enemies[0].isShot) {
+                example.bullets[0].y -= 12;
+                if (example.bullets[0].y < 0) {
+                    example.bullets[0].isShot = false;
+                }
+            }
+
+            // shoot if we're close to shot position
+            if (Math.abs(example.enemies[0].x - example.enemies[0].shotPosition) < 5 && !example.enemies[0].isGunLoaded) {
+                // shoot
+                example.enemies[0].isShot = true;
+                // reload a gun
+                example.enemies[0].isGunLoaded = true;
+            }
+            /// end
 
 
             myTxt.text = 'score: ' + example.score;
