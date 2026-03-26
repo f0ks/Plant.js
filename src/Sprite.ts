bb4c968fbe140d34ffd38149b4b633f0690b503f
Albert <albert.khm@gmail.com>
2026-03-26 20:11:19 +0100

    migrated `Sprite` module to use private fields and accessor properties for dimensions (width, height, frameWidth, frameHeight). [fixed bug with no sprites displayed on first load]

--- src/Sprite.ts
+++ src/Sprite.ts
@@ -3,10 +3,6 @@ import type { SceneNode, SpriteOptions } from "./types";
 export class Sprite implements SceneNode {
   node: HTMLImageElement;
   src: string;
-  width: number;
-  height: number;
-  frameWidth: number;
-  frameHeight: number;
   xFrame: number;
   yFrame: number;
   x: number;
@@ -23,15 +19,20 @@ export class Sprite implements SceneNode {
   /** @internal */
   _fadingFrame: number | null = null;
 
+  private _width: number | null;
+  private _height: number | null;
+  private _frameWidth: number | null;
+  private _frameHeight: number | null;
+
   constructor(options: SpriteOptions) {
     this.node = new Image();
     this.src = options.src;
     this.node.src = options.src;
 
-    this.width = options.width ?? this.node.width;
-    this.height = options.height ?? this.node.height;
-    this.frameWidth = options.frameWidth ?? this.node.width;
-    this.frameHeight = options.frameHeight ?? this.node.height;
+    this._width = options.width ?? null;
+    this._height = options.height ?? null;
+    this._frameWidth = options.frameWidth ?? null;
+    this._frameHeight = options.frameHeight ?? null;
 
     this.xFrame = options.xFrame ?? 0;
     this.yFrame = options.yFrame ?? 0;
@@ -40,6 +41,38 @@ export class Sprite implements SceneNode {
     this.opacity = options.opacity ?? 1;
     this.zindex = options.zindex ?? 1;
     this.visible = options.visible ?? true;
+  }
+
+  get width(): number {
+    return this._width ?? this.node.naturalWidth;
+  }
+
+  set width(value: number) {
+    this._width = value;
+  }
+
+  get height(): number {
+    return this._height ?? this.node.naturalHeight;
+  }
+
+  set height(value: number) {
+    this._height = value;
+  }
+
+  get frameWidth(): number {
+    return this._frameWidth ?? this.node.naturalWidth;
+  }
+
+  set frameWidth(value: number) {
+    this._frameWidth = value;
+  }
+
+  get frameHeight(): number {
+    return this._frameHeight ?? this.node.naturalHeight;
+  }
+
+  set frameHeight(value: number) {
+    this._frameHeight = value;
   }
 
   type(): string {
